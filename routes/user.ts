f084f6f107846d23c9717c8781d3259091c98104
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-11-05 19:18:48 -0300

    [add] updateUser

--- routes/user.ts
+++ routes/user.ts
@@ -1,12 +1,14 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
+import { z, ZodError } from "zod";
 
 const prisma = new PrismaClient();
 
 export async function getUserRoutes(app: FastifyInstance) {
   app.get("/", getUsers);
+  app.get("/:nome", getUserName);
   app.post("/", createUserHandler);
+  app.put("/:id", updateUserHandler);
 }
 
 async function getUsers(request: FastifyRequest, reply: FastifyReply) {
@@ -16,6 +18,33 @@ async function getUsers(request: FastifyRequest, reply: FastifyReply) {
   } catch (error) {
     console.error(error);
     reply.status(500).send({ error: "Erro ao buscar usuários" });
+  }
+}
+
+async function getUserName(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const { nome } = request.params as { nome: string };
+
+    const user = await prisma.cadastroAluno.findFirst({
+      where: {
+        nome: nome,
+      },
+    });
+
+    if (!user) {
+      return reply.status(404).send({ error: "Usuário não encontrado" });
+    }
+
+    return reply.send(user);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return reply.status(400).send({
+        error: "Erro de validação",
+        issues: error.errors,
+      });
+    }
+    console.error(error);
+    return reply.status(500).send({ error: "Erro ao buscar o usuário" });
   }
 }
 
@@ -42,6 +71,70 @@ async function createUserHandler(request: FastifyRequest, reply: FastifyReply) {
 
     return reply.status(201).send(user);
   } catch (error) {
-    return reply.status(400).send(error);
+    if (error instanceof ZodError) {
+      return reply.status(400).send({
+        error: "Erro de validação",
+        issues: error.errors,
+      });
+    }
+    console.error(error);
+    return reply.status(500).send({ error: "Erro ao criar usuário" });
+  }
+}
+
+async function updateUserHandler(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const { id } = request.params as { id: string };
+    const parsedId = parseInt(id);
+
+    if (Number.isNaN(parsedId)) {
+      return reply
+        .status(400)
+        .send({ error: "ID inválido. O ID deve ser um número." });
+    }
+
+    const createUserSchema = z.object({
+      nome: z.string(),
+      sobrenome: z.string(),
+      idade: z.number(),
+      endereco: z.string(),
+      email: z.string().email(),
+      peso: z.number(),
+      altura: z.number(),
+      planoEscolhidoId: z.number(),
+      planoAtivo: z.number(),
+      dataPagamento: z.string(),
+    });
+
+    const userData = createUserSchema.parse(request.body);
+
+    const updateUser = await prisma.cadastroAluno.update({
+      where: {
+        id: parsedId,
+      },
+      data: {
+        nome: userData.nome,
+        sobrenome: userData.sobrenome,
+        idade: userData.idade,
+        endereco: userData.endereco,
+        email: userData.email,
+        peso: userData.peso,
+        altura: userData.altura,
+        planoEscolhidoId: userData.planoEscolhidoId,
+        planoAtivo: userData.planoAtivo,
+        dataPagamento: userData.dataPagamento,
+      },
+    });
+
+    reply.send(updateUser);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return reply.status(400).send({
+        error: "Erro de validação",
+        issues: error.errors,
+      });
+    }
+    console.error(error);
+    return reply.status(500).send({ error: "Erro ao editar usuário" });
   }
 }
